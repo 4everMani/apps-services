fd70b94f237bc4c7b4e57c42ba5def865f42b60a
4evermani <jaiswal.manish49@gmail.com>
2024-03-12 12:13:41 +0530

    established database connection

---
+++ src/DataAccess/Northwind.Console.SqlClient/Program.Helpers.cs
@@ -0,0 +1,23 @@
+﻿using System.Globalization; // To use CultureInfo
+
+partial class Program
+{
+    private static void ConfigureConsole(string culture = "en-Us", bool useComputerCulture = false)
+    {
+        // To enable Unicode charaters like Euro symbol in the console
+        OutputEncoding = System.Text.Encoding.UTF8;
+        if (!useComputerCulture)
+        {
+            CultureInfo.CurrentCulture = CultureInfo.GetCultureInfo(culture);
+        }
+        WriteLine($"CultureInfo: {CultureInfo.CurrentCulture.DisplayName}");
+    }
+
+    private static void WriteLineInColor(string value, ConsoleColor color = ConsoleColor.White)
+    {
+        ConsoleColor previousColor = ForegroundColor;
+        ForegroundColor = color;
+        WriteLine(value);
+        ForegroundColor = previousColor;
+    }
+}
---
+++ src/DataAccess/Northwind.Console.SqlClient/Program.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.Data.SqlClient;
+using System.Data;
+
+ConfigureConsole();
+
+#region Set up the connection string builder
+SqlConnectionStringBuilder builder = new()
+{
+    InitialCatalog = "Northwind",
+    MultipleActiveResultSets = true,
+    TrustServerCertificate = true,
+    IntegratedSecurity = true,
+    ConnectTimeout = 10, // Default is 30 sec.
+    DataSource = "HACKDAMN"
+};
+
+#endregion
+
+#region Create and open the connection
+SqlConnection conneciton = new(builder.ConnectionString);
+WriteLine(conneciton.ConnectionString);
+WriteLine();
+conneciton.StateChange += Connection_StateChange;
+conneciton.InfoMessage += Connection_InfoMessage;
+
+try
+{
+    WriteLine("Opening connection. Please wait up to {0} seconds...", builder.ConnectTimeout);
+    WriteLine();
+    conneciton.Open();
+    WriteLine($"SQL Server version: {conneciton.ServerVersion}");
+}
+catch (SqlException ex)
+{
+
+    WriteLineInColor($"SQL exception: {ex.Message}", ConsoleColor.Red);
+    return;
+}
+#endregion
+
+conneciton.Close();
